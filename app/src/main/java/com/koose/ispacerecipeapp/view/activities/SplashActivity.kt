7b70d9e6972a0467de06a62c5c9449504ca9bab2
Koose <KooseTheBlogger@gmail.com>
2022-03-14 18:54:47 +0000

    Android Manifest for SplashScreen

---
+++ app/src/main/java/com/koose/ispacerecipeapp/view/activities/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.koose.ispacerecipeapp.view.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.koose.ispacerecipeapp.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
