208a0f4c098eb6633c4da3b080f0e9ce3bbae1e9
Koose <KooseTheBlogger@gmail.com>
2022-03-14 19:22:10 +0000

    splash screen and logos created

--- app/src/main/java/com/koose/ispacerecipeapp/view/activities/SplashActivity.kt
+++ app/src/main/java/com/koose/ispacerecipeapp/view/activities/SplashActivity.kt
@@ -1,12 +1,27 @@
 package com.koose.ispacerecipeapp.view.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.koose.ispacerecipeapp.MainActivity
 import com.koose.ispacerecipeapp.R
+import com.koose.ispacerecipeapp.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+
+    //institte view binding
+    private lateinit var binding: ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        // Handle the splash screen transition.
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        // Keep the splash screen visible for this Activity
+        splashScreen.setKeepOnScreenCondition { true }
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
