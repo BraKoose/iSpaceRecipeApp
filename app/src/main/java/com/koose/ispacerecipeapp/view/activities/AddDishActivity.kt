bba992e98e348c08f3d63cc512f5cf41a149632a
Koose <KooseTheBlogger@gmail.com>
2022-03-16 19:09:26 +0000

    @ dimen 200 on AddDish

---
+++ app/src/main/java/com/koose/ispacerecipeapp/view/activities/AddDishActivity.kt
@@ -0,0 +1,12 @@
+package com.koose.ispacerecipeapp.view.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.koose.ispacerecipeapp.R
+
+class AddDishActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_dish)
+    }
+}
