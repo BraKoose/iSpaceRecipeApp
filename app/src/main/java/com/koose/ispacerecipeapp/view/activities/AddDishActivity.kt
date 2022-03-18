843a49b63e76a60658acc6253a2809799395502f
Koose <KooseTheBlogger@gmail.com>
2022-03-18 19:08:42 +0000

    companion object

--- app/src/main/java/com/koose/ispacerecipeapp/view/activities/AddDishActivity.kt
+++ app/src/main/java/com/koose/ispacerecipeapp/view/activities/AddDishActivity.kt
@@ -1,11 +1,17 @@
 package com.koose.ispacerecipeapp.view.activities
 
+import android.Manifest
 import android.app.Dialog
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.koose.ispacerecipeapp.R
+import androidx.appcompat.app.AppCompatActivity
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 import com.koose.ispacerecipeapp.databinding.ActivityAddDishBinding
 import com.koose.ispacerecipeapp.databinding.PickImgCameraGalleryBinding
+
 
 class AddDishActivity : AppCompatActivity() {
 
@@ -24,20 +30,42 @@ class AddDishActivity : AppCompatActivity() {
         }
     }
 
-    private fun openCameraGalleryDialog(){
+    private fun openCameraGalleryDialog() {
         dialog = Dialog(this)
         bindingPickImg = PickImgCameraGalleryBinding.inflate(layoutInflater)
         dialog.setContentView(bindingPickImg.root)
 
         //set onClickon the Camera
         bindingPickImg.pictImgCamera.setOnClickListener {
+            Dexter.withContext(this)
+                .withPermissions(
+                    Manifest.permission.CAMERA,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                ).withListener(object : MultiplePermissionsListener {
+                    override fun onPermissionsChecked(report: MultiplePermissionsReport) { /* ... */
+
+                        if (report.areAllPermissionsGranted()) {
+                            val take
+                        }
+                    }
+
+                    override fun onPermissionRationaleShouldBeShown(
+                        permissions: List<PermissionRequest>,
+                        token: PermissionToken
+                    ) { /* ... */
+                    }
+                }).check()
+
+            bindingPickImg.pickImgGallery.setOnClickListener {
+
+            }
+
+            dialog.show()
+
 
         }
-
-        bindingPickImg.pickImgGallery.setOnClickListener {
-
-        }
-
-        dialog.show()
+    }
+    companion object{
+        const val REQUEST_IMAGE_CAPTURE
     }
 }
