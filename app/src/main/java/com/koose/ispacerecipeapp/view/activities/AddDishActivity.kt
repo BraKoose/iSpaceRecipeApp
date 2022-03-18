37bf308c85becf6438698e454ad201493b988270
Koose <KooseTheBlogger@gmail.com>
2022-03-18 18:54:47 +0000

    manifest file

--- app/src/main/java/com/koose/ispacerecipeapp/view/activities/AddDishActivity.kt
+++ app/src/main/java/com/koose/ispacerecipeapp/view/activities/AddDishActivity.kt
@@ -1,12 +1,43 @@
 package com.koose.ispacerecipeapp.view.activities
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.koose.ispacerecipeapp.R
+import com.koose.ispacerecipeapp.databinding.ActivityAddDishBinding
+import com.koose.ispacerecipeapp.databinding.PickImgCameraGalleryBinding
 
 class AddDishActivity : AppCompatActivity() {
+
+    private lateinit var bindingActivityDish: ActivityAddDishBinding
+    lateinit var bindingPickImg: PickImgCameraGalleryBinding
+        private lateinit var dialog:Dialog
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_dish)
+        bindingActivityDish = ActivityAddDishBinding.inflate(layoutInflater
+        )
+        setContentView(bindingActivityDish.root)
+
+        bindingActivityDish.ivAddDishImage.setOnClickListener {
+            openCameraGalleryDialog()
+        }
+    }
+
+    private fun openCameraGalleryDialog(){
+        dialog = Dialog(this)
+        bindingPickImg = PickImgCameraGalleryBinding.inflate(layoutInflater)
+        dialog.setContentView(bindingPickImg.root)
+
+        //set onClickon the Camera
+        bindingPickImg.pictImgCamera.setOnClickListener {
+
+        }
+
+        bindingPickImg.pickImgGallery.setOnClickListener {
+
+        }
+
+        dialog.show()
     }
 }
